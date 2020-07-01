130c7c8278f2ed87e524f79d63c7cdb8340b9efd
HYACINTHE <hyacinthe@localhost.localdomain>
2020-07-01 05:41:40 +0100

    test

--- test.sh
+++ test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-sudo cp conf/test /etc/
+name=$(echo $1 | cut -f1 -d'.')
+temp=$(awk "/$name/ {print}" forward.dev.irex.aretex.ca.db | head -n1 | awk '{print $4}')
+
+
+if [[ $temp = "" ]]
+then
+	echo "Ce nom n'existe pas il vas etre renseigne"
+else
+	sed "s/$temp/$2/g" forward.dev.irex.aretex.ca.db
+	echo "succefull modify"
+fi
+
