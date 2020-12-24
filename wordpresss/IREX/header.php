c2a9d696f85dfde8fda8dc3c287aa9aa83ec0a8b
Darlin Hyacinthe <darlinteuma2@gmail.com>
2020-12-24 15:24:47 +0100

    a

--- wordpresss/IREX/header.php
+++ wordpresss/IREX/header.php
@@ -61,42 +61,5 @@
             </ul>
 
         </div>
-
-        <?php
-    wp_nav_menu([
-      'menu'            => 'primary',
-      'theme_location'  => 'menu-1',
-      'container'       => 'div',
-      'container_id'    => 'navbarCollapse',
-      // 'container_class' => 'collapse navbar-collapse',
-      'menu_id'         => false,
-      'menu_class'      => 'navbar-nav mr-auto',
-      'depth'           => 0,
-      'fallback_cb'     => 'bs4navwalker::fallback',
-      'walker'          => new bs4navwalker()
-    ]);
-  ?> 
     </div>
 </div>
-
-
-
-
-
-
-<!-- <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-  <a class="navbar-brand" href="#">
-    <?php bloginfo('name'); ?>
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-</nav> -->
-
-
-
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
