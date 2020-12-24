6867f32502aca77d25a2c77c2dfc8a84c9d3b815
Darlin Hyacinthe <darlinteuma2@gmail.com>
2020-12-24 20:36:43 +0100

    a

--- wordpresss/IREX/header.php
+++ wordpresss/IREX/header.php
@@ -20,7 +20,7 @@
 <body <?php body_class(); ?>>
 
 
-<nav class="navbar navbar-light bg-light navbar-expand-lg fixed-top" style="height: 25px">
+<nav class="navbar navbar-light bg-light navbar-expand-lg fixed-top" style="height: 25px; background-color: black !important;">
   <a href="#" class="navbar-brand"></a>
   <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
     <span class="navbar-toggler-icon"></span>
@@ -28,16 +28,16 @@
   <div class="collapse navbar-collapse" id="navbarCollapse">
     <ul class="navbar-nav ml-auto">
       <li clas="navbar-item">
-        <a href="#" class="nav-link">Homepage</a>
+        <a href="#" class="nav-link" style="color: white !important; ">Homepage</a>
         </li>
         <li clas="navbar-item">
-          <a href="#" class="nav-link">Blog</a>
+          <a href="#" class="nav-link" style="color: white !important; ">Blog</a>
           </li>
         <li clas="navbar-item">
-          <a href="#" class="nav-link">About</a>
+          <a href="#" class="nav-link" style="color: white !important; ">About</a>
       </li>  
       <li clas="navbar-item">
-          <a href="#" class="nav-link">Contact</a>
+          <a href="#" class="nav-link" style="color: white !important; ">Contact</a>
       </li>
     </ul>
   </div>
