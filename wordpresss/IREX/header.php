e592910beef27dec18a5f9a15b8f59a152c2cc92
Darlin Hyacinthe <darlinteuma2@gmail.com>
2020-12-28 08:27:22 +0100

    a

--- wordpresss/IREX/header.php
+++ wordpresss/IREX/header.php
@@ -22,9 +22,6 @@
 
 <nav class="navbar navbar-light bg-light navbar-expand-lg fixed-top" style="height: 25px; background-color: #202020 !important;z-index: 9999;">
   <a href="#" class="navbar-brand"></a>
-  <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-    <span class="navbar-toggler-icon"></span>
-  </button>
   <div class="collapse navbar-collapse" id="navbarCollapse">
     <ul class="navbar-nav ml-auto">
       <li clas="navbar-item">
