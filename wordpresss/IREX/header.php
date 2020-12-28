f80daccdb7eefa055edff7ce3feac670b326607c
Darlin Hyacinthe <darlinteuma2@gmail.com>
2020-12-28 09:25:09 +0100

    a

--- wordpresss/IREX/header.php
+++ wordpresss/IREX/header.php
@@ -49,19 +49,13 @@
 </nav> -->
 
 
-<div class="navbar">
-  <a href="#home">Home</a>
-  <a href="#news">News</a>
-  <div class="dropdown">
-    <button class="dropbtn">Dropdown 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
-    </div>
-  </div> 
+<div class="dropdown">
+  <button class="dropbtn">Name</button>
+  <div class="dropdownContent">
+    <a href="#">One</a>
+    <a href="#">Two</a>
+    <a href="#">Three</a>
+  </div>
 </div>
 
 
