cb57382a8a66dda302d43ea23f6839fca4a7ed9c
darlinho <darlinteuma2@gmail.com>
2021-04-05 11:55:10 +0100

    dqd

--- edit-conf.sh
+++ edit-conf.sh
@@ -2,52 +2,24 @@
 
 WP_HOME=$HOME/wordpress/docker
 
-################### For wordpress install ##############################
-
-IREX_DOMAIN=dev.irex.aretex.ca
-MYSQL_PASSWORD=w0rdpr355-d3V
-WP_URL=wp.dev.irex.aretex.ca
-WP_DOMAINS="wp.dev.irex.aretex.ca"
-
-
-################### For step-ca configuration ###########################
-
-##get the step server hostname
-STEP_CA_HOST="step-ca.uat.irex.aretex.ca"
-
-##get the step client server
-STEP_CLIENT_NAME=$SERVER_NAME
-
-##get the IP of step server
-STEP_CA_IP="192.168.33.202"
-
-#get the port for access to step-ca service, make sure it's the correct value with step-server
-PORT_ACCESS=8443
-
-##get the user who has installed step-ca server
-STEP_CA_USER="debian"
-
-##get the pass of user
-STEP_CA_PASS_USER="debian"
-
 
 sed -i "s/IREX_DOMAIN=.*/IREX_DOMAIN=$IREX_DOMAIN/g" $WP_HOME/conf.defaults
 
-sed -i "s/MYSQL_PASSWORD=.*/IREX_DOMAIN=$IREX_DOMAIN/g" $WP_HOME/conf.defaults
+sed -i "s/MYSQL_PASSWORD=.*/MYSQL_PASSWORD=$MYSQL_PASSWORD/g" $WP_HOME/conf.defaults
 
-sed -i "s/IREX_DOMAIN=.*/IREX_DOMAIN=$IREX_DOMAIN/g" $WP_HOME/conf.defaults
+sed -i "s/WP_URL=.*/WP_URL=$WP_URL/g" $WP_HOME/conf.defaults
 
-sed -i "s/IREX_DOMAIN=.*/IREX_DOMAIN=$IREX_DOMAIN/g" $WP_HOME/conf.defaults
+sed -i "s/WP_DOMAINS=.*/WP_DOMAINS=$WP_DOMAINS/g" $WP_HOME/conf.defaults
 
-sed -i "s/IREX_DOMAIN=.*/IREX_DOMAIN=$IREX_DOMAIN/g" $WP_HOME/conf.defaults
+sed -i "s/STEP_CA_HOST=.*/STEP_CA_HOST=$STEP_CA_HOST/g" $WP_HOME/conf.defaults
 
-sed -i "s/IREX_DOMAIN=.*/IREX_DOMAIN=$IREX_DOMAIN/g" $WP_HOME/conf.defaults
+sed -i "s/STEP_CLIENT_NAME=.*/STEP_CLIENT_NAME=$STEP_CLIENT_NAME/g" $WP_HOME/conf.defaults
 
-sed -i "s/IREX_DOMAIN=.*/IREX_DOMAIN=$IREX_DOMAIN/g" $WP_HOME/conf.defaults
+sed -i "s/STEP_CA_IP=.*/STEP_CA_IP=$STEP_CA_IP/g" $WP_HOME/conf.defaults
 
-sed -i "s/IREX_DOMAIN=.*/IREX_DOMAIN=$IREX_DOMAIN/g" $WP_HOME/conf.defaults
+sed -i "s/PORT_ACCESS=.*/PORT_ACCESS=$PORT_ACCESS/g" $WP_HOME/conf.defaults
 
-sed -i "s/IREX_DOMAIN=.*/IREX_DOMAIN=$IREX_DOMAIN/g" $WP_HOME/conf.defaults
+sed -i "s/STEP_CA_USER=.*/STEP_CA_USER=$STEP_CA_USER/g" $WP_HOME/conf.defaults
 
-sed -i "s/IREX_DOMAIN=.*/IREX_DOMAIN=$IREX_DOMAIN/g" $WP_HOME/conf.defaults
+sed -i "s/STEP_CA_PASS_USER=.*/STEP_CA_PASS_USER=$STEP_CA_PASS_USER/g" $WP_HOME/conf.defaults
 
